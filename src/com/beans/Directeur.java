234e245cbfc79de2b7bda4e96222fd955245b25a
redouane07 <fr_bouras@esi.dz>
2017-02-28 17:21:15 +0100

    Historique pointage Afficher les information d'un employee

--- src/com/beans/Directeur.java
+++ src/com/beans/Directeur.java
@@ -1,7 +1,113 @@
 package com.beans;
 
-public class Directeur extends EmployeGeneral implements LienDirChefDep {
+import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+
+import org.primefaces.model.chart.LineChartModel;
+
+@ManagedBean
 
 
-//ceci 
+public class Directeur extends EmployeGeneral  {
+
+
+	private List<Enter> entrants = loadusers();
+	
+	private List<Enter> filteredEntrants ;
+	
+
+
+	public List<Enter> getFilteredEntrants() {
+		return filteredEntrants;
+	}
+
+	public void setFilteredEntrants(List<Enter> filteredEntrants) {
+		this.filteredEntrants = filteredEntrants;
+	}
+
+	public List<Enter> getEntrants() {
+		return entrants;
+	}
+
+	public void setEntrants(List<Enter> entrants) {
+		this.entrants = entrants;
+	}
+
+	public List<Enter> loadusers()
+	{
+		
+		List<Enter> tab = new ArrayList<Enter>();
+		PreparedStatement statement = null;
+        ResultSet resultat = null;
+        Connection connexion = null;
+        connexion = Database.loadDatabase();      
+        try {
+            statement = connexion.prepareStatement("SELECT * FROM pointage ORDER BY matricule ;");
+
+            // Exécution de la requête
+
+            resultat = statement.executeQuery();
+            
+            // Récupération des données
+        	
+            while (resultat.next())
+            {                          
+            	Enter user = new Enter ();
+                user.setMatricule(resultat.getInt("matricule"));
+                user.setDate(resultat.getDate("jour"));
+                user.setTime(resultat.getTime("heure_pointage"));
+                statement = connexion.prepareStatement("SELECT * FROM employee WHERE matricule = ? ;");
+                
+                statement.setInt(1, resultat.getInt("matricule"));
+
+                // Exécution de la requête
+
+                ResultSet resultat1 = statement.executeQuery();
+                if(resultat1.next())
+                {
+                    user.setNom(resultat1.getString("nom"));
+                    user.setPrenom(resultat1.getString("prenom"));
+                }
+
+                tab.add(user) ;
+                
+            }
+            
+        
+        }
+        catch (SQLException e) {
+
+        } finally {
+
+            // Fermeture de la connexion
+
+            try {
+
+                if (resultat != null)
+                    resultat.close();
+
+                if (statement != null)
+                    statement.close();
+
+                if (connexion != null)
+                    connexion.close();
+
+            } catch (SQLException ignore) {
+            }
+        }
+		return tab;
+	}
+	
+	
 }
--- src/com/beans/Employe.java
+++ src/com/beans/Employe.java
@@ -1,9 +1,143 @@
 package com.beans;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+import javax.faces.context.FacesContext;
+import javax.servlet.http.HttpSession;
+
+@ManagedBean
+@SessionScoped
 public class Employe extends EmployeGeneral {
 	protected String nomService;
 	protected String nomDepartement;
 	
+	public String getNomService() {
+		return nomService;
+	}
+	public void setNomService(String nomService) {
+		this.nomService = nomService;
+	}
+	public String getNomDepartement() {
+		return nomDepartement;
+	}
+	public void setNomDepartement(String nomDepartement) {
+		this.nomDepartement = nomDepartement;
+	}
+	
+
+	@PostConstruct
+	public void showInfo(){
+		PreparedStatement statement = null;
+        ResultSet resultat = null;
+        Connection connexion = null;
+        connexion = Database.loadDatabase();      
+        try 
+        {
+            statement = connexion.prepareStatement("SELECT * FROM employee where matricule= ?;");
+    		HttpSession session = (HttpSession) FacesContext.getCurrentInstance()
+    				.getExternalContext().getSession(false);
+    		
+    		statement.setString(1, session.getAttribute("matricule").toString());
+            resultat = statement.executeQuery();
+            
+        	
+        	    int service_id=1 ,grade_id=0,role_id=0,departement_id=0;
+
+
+                if(resultat.next() );  
+                {
+                	matricule = (resultat.getInt("matricule"));
+                    nom = (resultat.getString("nom"));
+                    prenom = (resultat.getString("prenom"));
+                    service_id=resultat.getInt("service_id");
+                    grade_id=resultat.getInt("grade_id");
+                    departement_id=resultat.getInt("departement_id") ;
+                    role_id = resultat.getInt("role_id") ;
+                    salaire = (resultat.getFloat("salaire"));              
+
+                    
+	                if (service_id != 0)
+	                {
+	                	
+	                   statement =connexion.prepareStatement("SELECT *  FROM service WHERE id = ? ;");
+	            	
+	                 	statement.setInt(1,service_id);           
+	                   resultat = statement.executeQuery();
+	                
+	                                        
+	                    resultat.next() ;                          
+	                    nomService = (resultat.getString("nom_service")) ;
+	                }
+	                else
+	                {
+	                	nomService = "aucun service";
+	                }
+                
+                
+	               if (grade_id != 0)
+	                {
+	                   statement =connexion.prepareStatement("SELECT *  FROM grade WHERE id = ? ;");
+	            	
+	                 	statement.setInt(1,grade_id);           
+	                   resultat = statement.executeQuery();
+	                                                    
+	                    resultat.next() ;                          
+	                    grade = resultat.getString("grade") ;
+	                }
+	                else
+	                {
+	                    grade = "aucun grade" ;
+	                }
+               
+               
+	               if (role_id != 0)
+	               {
+	                  statement =connexion.prepareStatement("SELECT * FROM role WHERE id = ? ;");
+	           	
+	                	statement.setInt(1,role_id);           
+	                  resultat = statement.executeQuery();
+	                                                   
+	                   resultat.next() ;                          
+	                   role = resultat.getString("nom_role");
+	               }
+	               else
+	               {
+	                   role = "aucun role" ;
+	               }
+               
+               
+	               if (departement_id != 0)
+	               {
+	                  statement =connexion.prepareStatement("SELECT *  FROM departement WHERE id = ? ;");
+	           	
+	                	statement.setInt(1,departement_id);           
+	                  resultat = statement.executeQuery();
+	                                                   
+	                   resultat.next() ;                          
+	                   nomDepartement= resultat.getString("nom_departement") ;
+	               }
+	               else
+	               {
+	                   nomDepartement = "aucun departement" ;
+	               }
+                }                                      
+                                    
+      }catch (SQLException e) {}
+      finally 
+      {
+             try 
+    	        {
+                 
+                if (connexion != null) connexion.close();                   
+                } catch (SQLException ignore) {}
+        }
+	}
 	//public <type> monService();
 	//public <type> monDepartement();
 	
