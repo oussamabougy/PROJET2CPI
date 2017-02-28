3916ef662cbda7a4a01fa6ba5429493a14a91c69
redouane07 <fr_bouras@esi.dz>
2017-02-28 17:32:35 +0100

    Les plug-in qui permet d'exporter les listes sous forme d'un fichier PDF, EXCEL ou CSV

---
+++ src/com/beans/Enter.java
@@ -0,0 +1,51 @@
+package com.beans;
+
+import java.sql.Date;
+import java.sql.Time;
+
+
+import com.sun.msv.datatype.xsd.DateType;
+
+
+public class Enter {
+	
+	private Date date ;
+	private Time time ;
+	private int matricule ;
+	private String nom;
+	private String prenom;
+	
+	
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	public int getMatricule() {
+		return matricule;
+	}
+	public void setMatricule(int matricule) {
+		this.matricule = matricule;
+	}
+	public Time getTime() {
+		return time;
+	}
+	public void setTime(Time time) {
+		this.time = time;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	
+	
+}
