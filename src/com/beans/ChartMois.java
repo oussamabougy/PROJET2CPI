b78838f9e2cac22ef36e54f83b1f2fe94f860e3e
lemsi98 <fi_neddar@esi.dz>
2017-03-03 21:37:36 +0000

    je l'ai oublie Chart Mois

---
+++ src/com/beans/ChartMois.java
@@ -0,0 +1,21 @@
+package com.beans;
+
+public class ChartMois {
+	private int Mois;
+	private int jour_absence;
+	public int getMois() {
+		return Mois;
+	}
+	public void setMois(int mois) {
+		Mois = mois;
+	}
+	public int getJour_absence() {
+		return jour_absence;
+	}
+	public void setJour_absence(int jour_absence) {
+		this.jour_absence = jour_absence;
+	}
+
+	
+	
+}
